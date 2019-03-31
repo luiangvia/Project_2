bf5d8597cb91f72c43abfe62e94327d31d9eabbd
Boki23m <boki23usa@yahoo.com>
2019-03-30 18:53:25 -0700

    add recipe to list function fixed

--- public/js/view.js
+++ public/js/view.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   //TODO: Change naming convention from 'todo' to 'recipe'
 
   // Getting a reference to the input field where user adds a new recipe
-  var $newItemInput = $("input.new-item");
+
   // Our new recipes will go inside the todoContainer
   var $todoContainer = $(".todo-container");
   // Adding event listeners for deleting, editing, and adding recipes
   $(document).on("click", "button.delete", deleteTodo);
 
-  $(document).on("submit", "#todo-form", insertTodo);
+  $(document).on("click", "button.addRecipe", insertTodo);
 
   // Our initial recipes array
   var todos = [];
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
   // This function grabs recipes from the database and updates the view
   function getTodos() {
-    $.get("/api/todos", function(data) {
+    $.get("/api/todos", function (data) {
       todos = data;
       initializeRows();
     });
@@ -45,18 +45,16 @@ $(document).ready(function() {
     }).then(getTodos);
   }
 
- 
+
   // This function constructs a recipe-item row
   function createNewRow(todo) {
     var $newInputRow = $(
       [
-        "<li class='list-group-item todo-item'>",
-        "<span>",
+        "<li class='list-group-item d-flex justify-content-between align-items-center todo-item'>",
+        "<h4>",
         todo.text,
-        "</span>",
-        "<input type='text' class='edit' style='display: none;'>",
-        "<button class='delete btn btn-danger'>x</button>",
-        
+        "</h4>",
+        "<button class='delete btn btn-danger'>Remove</button>",
         "</li>"
       ].join("")
     );
@@ -73,12 +71,14 @@ $(document).ready(function() {
   // This function inserts a new recipe into our database and then updates the view
   function insertTodo(event) {
     event.preventDefault();
+    var $newItemInput = $(this).val();
+    console.log($newItemInput);
     var todo = {
-      text: $newItemInput.val().trim(),
+      text: $newItemInput,
       complete: false
     };
 
     $.post("/api/todos", todo, getTodos);
-    $newItemInput.val("");
+    $newItemInput;
   }
 });
